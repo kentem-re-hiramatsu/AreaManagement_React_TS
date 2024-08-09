4a61f6ee0ac2589b627bdc59eb2a6e09eed00953
re-hiramatsu <re-hiramatsu>
2024-08-09 14:21:57 +0900

    Header実装

---
+++ AreaManagement/src/views/header/Header.tsx
@@ -0,0 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+import '../../styles/header/header.css';
+
+export const Header = () => {
+    const navigate = useNavigate();
+
+    const handleHomeClick = () => {
+        navigate('/');
+    };
+
+    const handleAddClick = () => {
+        navigate('/add');
+    };
+
+    return (
+        <header>
+            <div className='btn add-btn' onClick={handleHomeClick}>ホーム</div>
+            <div className='btn add-btn' onClick={handleAddClick}>追加</div>
+        </header>
+    )
+}
