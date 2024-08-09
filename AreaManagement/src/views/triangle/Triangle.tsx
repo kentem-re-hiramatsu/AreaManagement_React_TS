0423e39984acf2fb3e5e4059b9be1d6c1272329b
re-hiramatsu <re-hiramatsu>
2024-08-09 14:29:21 +0900

    三角形の辺の入力欄を実装

---
+++ AreaManagement/src/views/triangle/Triangle.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+type propsType = {
+    handleBHChange: (newBaseLength: number, newHeight: number) => void;
+}
+
+export const Triangle: React.FC<propsType> = ({ handleBHChange }) => {
+    const [baseLength, setBaseLength] = useState<number>(0);
+    const [height, setHeight] = useState<number>(0);
+
+    const handleBaseChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newBaseLength = Number(e.target.value);
+        setBaseLength(newBaseLength);
+        handleBHChange(newBaseLength, height);
+    };
+
+    const handleHeightChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newHeight = Number(e.target.value);
+        setHeight(newHeight);
+        handleBHChange(baseLength, newHeight);
+    };
+
+    return (
+        <>
+            <label>
+                底辺
+                <input type="number" onChange={handleBaseChange} />
+            </label>
+            <label>
+                高さ
+                <input type="number" onChange={handleHeightChange} />
+            </label>
+        </>
+    );
+}
