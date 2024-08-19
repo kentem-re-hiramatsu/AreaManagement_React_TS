8ed4e2a44453549883c081c4d3573fbbd9dd14e9
re-hiramatsu <re-hiramatsu>
2024-08-19 11:59:06 +0900

    辺の長さの入力値をuseStateからuseRefに変更

--- AreaManagement/src/components/Change.tsx
+++ AreaManagement/src/components/Change.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useRef } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { shapeNames } from "../consts/ShapeName";
 import { AreaContext } from '../provider/areaProvider';
@@ -6,26 +6,25 @@ import { circleType } from "../types/circle";
 import { quadrilareaType } from "../types/quadrilarea";
 import { triangleType } from "../types/traiangle";
 import { trapezoidType } from "../types/trapezoid";
-import { Circle } from "./Circle";
 import { Header } from "./Header";
-import { Quadrilarea } from "./Quadrilarea";
-import { Trapezoid } from "./Trapezoid";
-import { Triangle } from "./Triangle";
 
 export const Change = () => {
     const { id } = useParams();
     const navigate = useNavigate();
 
-    const context = useContext(AreaContext);
-    const { shapeData } = context!;
+    const { shapeData } = useContext(AreaContext)!;
     const shape = shapeData.find(shape => shape.id === id);
 
-    const [baseLength, setBaseLength] = useState<number>(0);
-    const [height, setHeight] = useState<number>(0);
-    const [upperBaseLength, setUpperBaseLength] = useState<number>(0);
+    const inputBaseLength = useRef<HTMLInputElement>(null);
+    const inputHeight = useRef<HTMLInputElement>(null);
+    const inputUpperBase = useRef<HTMLInputElement>(null);
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+
+        const baseLength: number = inputBaseLength.current ? parseInt(inputBaseLength.current.value) : 0;
+        const height: number = inputHeight.current ? parseInt(inputHeight.current.value) : 0;
+        const upperBaseLength: number = inputUpperBase.current ? parseInt(inputUpperBase.current.value) : 0;
 
         switch (shape!.shapeName) {
             case shapeNames.triangle:
@@ -57,21 +56,6 @@ export const Change = () => {
         navigate('/');
     };
 
-    const handleBChange = (newBaseLength: number) => {
-        setBaseLength(newBaseLength);
-    };
-
-    const handleBHChange = (newBaseLength: number, newHeight: number) => {
-        setBaseLength(newBaseLength);
-        setHeight(newHeight);
-    };
-
-    const handleBHUChange = (newBaseLength: number, newHeight: number, newUpperBaseLength: number) => {
-        setBaseLength(newBaseLength);
-        setHeight(newHeight);
-        setUpperBaseLength(newUpperBaseLength);
-    };
-
     return (
         <>
             <Header />
@@ -95,13 +79,53 @@ export const Change = () => {
                         {shapeNames.circle}
                     </label>
                 </section>
-                {shape!.shapeName === shapeNames.triangle && <Triangle handleBHChange={handleBHChange} />}
+                {shape!.shapeName === shapeNames.triangle && <>
+                    <label>
+                        底辺
+                        <input ref={inputBaseLength} type="number" min={0} />
+                    </label>
+                    <label>
+                        高さ
+                        <input ref={inputHeight} type="number" min={0} />
+                    </label>
+                </>
+                }
 
-                {shape!.shapeName === shapeNames.quadrilarea && <Quadrilarea handleBHChange={handleBHChange} />}
+                {shape!.shapeName === shapeNames.quadrilarea && <>
+                    <label>
+                        幅
+                        <input ref={inputBaseLength} type="number" min={0} />
+                    </label>
+                    <label>
+                        高さ
+                        <input ref={inputHeight} type="number" min={0} />
+                    </label>
+                </>
+                }
 
-                {shape!.shapeName === shapeNames.trapezoid && <Trapezoid handleBHUChange={handleBHUChange} />}
+                {shape!.shapeName === shapeNames.trapezoid && <>
+                    <label >
+                        上底
+                        <input ref={inputUpperBase} type="number" min={0} />
+                    </label>
+                    <label>
+                        下底
+                        <input ref={inputBaseLength} type="number" min={0} />
+                    </label>
+                    <label>
+                        高さ
+                        <input ref={inputHeight} type="number" min={0} />
+                    </label>
+                </>
+                }
 
-                {shape!.shapeName === shapeNames.circle && <Circle handleBChange={handleBChange} />}
+                {shape!.shapeName === shapeNames.circle && <>
+                    <label>
+                        半径
+                        <input ref={inputBaseLength} type="number" min={0} />
+                    </label>
+                </>
+                }
                 <section>
                     <button type="submit">決定</button>
                 </section>
